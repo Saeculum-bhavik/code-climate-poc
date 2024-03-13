02cb74e70ea6251bb345c3b8297146bb5cbe4837
bhaviksaeculum <bhavik.mojidra@saeculumsolutions.com>
2024-03-13 11:44:00 +0530

    nested if test

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function test(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11
   return "foo"
   debugger;
 }
+
+function nestedIfTest() {
+  if (true) {
+    if (1 == 1) {
+      if (2 == 2) {
+        return 2
+      }
+      return 1
+    }
+    return 3
+  }
+}
